57296fdf6cdbb64e1de8755ff4d879d76bc20976
J2Jayy <betatest0710@gmail.com>
2025-02-27 13:13:04 +0900

    FIX:알 DTO

--- ET-arcade/src/main/java/com/team1/etarcade/egg/dto/EggResponseDTO.java
+++ ET-arcade/src/main/java/com/team1/etarcade/egg/dto/EggResponseDTO.java
@@ -3,11 +3,15 @@ package com.team1.etarcade.egg.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class EggResponseDTO {
     private Long eggId;
     private Long userId;
-    private int remainingPoints;
-    private String message;
+    private boolean is_hatchable;
+    private boolean is_hatched;
+    private LocalDateTime createdAt;
+    private String timeRemaining;
 }
