6e9fba058150141d5d54ab88a411d17b0e49540d
DrivenByCode <jaykey1004@gmail.com>
2025-02-27 11:35:16 +0900

    fix: 의존성 문제 해결

--- ET-user/src/main/java/com/team1/etuser/user/controller/UserController.java
+++ ET-user/src/main/java/com/team1/etuser/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.team1.etuser.user.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
