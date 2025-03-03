43df5548a92914a44546f3fc4cdf8b8e0b4115a8
DrivenByCode <jaykey1004@gmail.com>
2025-03-03 23:39:32 +0900

    Feat: 로그인한 유저의 목장 조회

--- ET-arcade/src/main/java/com/team1/etarcade/pet/service/PetService.java
+++ ET-arcade/src/main/java/com/team1/etarcade/pet/service/PetService.java
@@ -1,7 +1,28 @@
 package com.team1.etarcade.pet.service;
 
+import com.team1.etarcade.pet.domain.Pet;
+import com.team1.etarcade.pet.dto.UserPetResponseDTO;
+import com.team1.etarcade.pet.connector.UserFeignConnector;
+import com.team1.etarcade.pet.repository.PetRepository;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PetService {
+
+    private final UserFeignConnector userFeignConnector;
+    private final PetRepository petRepository;
+
+    public List<Pet> getUserPets(Long userId) {
+        List<UserPetResponseDTO> userPets = userFeignConnector.getUserPets(userId);
+
+        return userPets.stream()
+                .map(userPet -> petRepository.findById(userPet.getPetId()).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
 }
