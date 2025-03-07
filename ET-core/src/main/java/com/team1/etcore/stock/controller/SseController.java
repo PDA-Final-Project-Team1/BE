cb716df8fc9f5277634ae33bbd4d339485dbc72a
DrivenByCode <jaykey1004@gmail.com>
2025-03-07 14:46:37 +0900

    feat: 거래 체결 알림

--- ET-core/src/main/java/com/team1/etcore/stock/controller/SseController.java
+++ ET-core/src/main/java/com/team1/etcore/stock/controller/SseController.java
@@ -1,7 +1,9 @@
 package com.team1.etcore.stock.controller;
 
 import com.team1.etcore.stock.service.SseService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -15,7 +17,6 @@ public class SseController {
 
     @GetMapping("/subscribe/interest-price")//관심종목 현재가
     public SseEmitter StockInterestPrice(@RequestHeader("X-Id") String userId) {
-
         return sseService.getInterestStockPrice(userId);
     }
     @GetMapping("/subscribe/portfolio-price")//보유 종목 현재가
@@ -25,11 +26,18 @@ public class SseController {
     @GetMapping("/ask-bid/{stockCode}")
     public SseEmitter StockAskBid(@PathVariable String stockCode) {
         return sseService.getAskBidPrice(stockCode);
-
     }
     @GetMapping("/cur-price/{stockCode}")
     public SseEmitter StockCurPrice(@PathVariable String stockCode) {
         return sseService.getStockCurPrice(stockCode);
     }
 
+    @GetMapping(value = "/subscribe/trade", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribeTrade(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Content-Type", "text/event-stream");
+
+        return sseService.subscribeTradeNotifications(1L);
+    }
 }
