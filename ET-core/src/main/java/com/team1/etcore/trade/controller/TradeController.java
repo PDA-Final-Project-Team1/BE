cd585a82a0551a26bb99089b6077c2c8552fe4d9
JeongTaek <jtkim965@naver.com>
2025-03-07 20:51:45 +0900

    Refactor/#73-Kafka를 이용해 정산하도록 수정

--- ET-core/src/main/java/com/team1/etcore/trade/controller/TradeController.java
+++ ET-core/src/main/java/com/team1/etcore/trade/controller/TradeController.java
@@ -17,16 +17,10 @@ public class TradeController {
 
     private final TradeService tradeService;
 
-    /**
-     * 매수 주문 생성 API
-     * @param userId 헤더에서 가져온 userId
-     * @param tradeReq 주문 요청 정보
-     * @return 생성된 주문 정보
-     */
+    // 매수 주문 생성 API
     @PostMapping("/buy")
     public TradeRes buy(@RequestHeader("X-Id") String userId, @RequestBody TradeReq tradeReq) {
         try {
-//            tradeReq.setUserId(Long.valueOf(userId));
             tradeReq.setPosition(Position.BUY);
             log.info("매수 주문 요청 수신 (사용자: {}): {}", userId, tradeReq);
             return tradeService.createOrder(Long.valueOf(userId), tradeReq);
@@ -36,12 +30,7 @@ public class TradeController {
         }
     }
 
-    /**
-     * 매도 주문 생성 API
-     * @param userId 헤더에서 가져온 userId
-     * @param tradeReq 주문 요청 정보
-     * @return 생성된 주문 정보
-     */
+    // 매도 주문 생성 API
     @PostMapping("/sell")
     public TradeRes sell(@RequestHeader("X-Id") String userId, @RequestBody TradeReq tradeReq) {
         try {
