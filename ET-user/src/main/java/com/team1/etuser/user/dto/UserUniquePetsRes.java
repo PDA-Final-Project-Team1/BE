a348ddfe8288abcfdbe560d77a7f9e5f987321ee
DrivenByCode <jaykey1004@gmail.com>
2025-03-09 21:08:05 +0900

    Feat: 도감 조회(고유한 펫 목록 반환)

---
+++ ET-user/src/main/java/com/team1/etuser/user/dto/UserUniquePetsRes.java
@@ -0,0 +1,14 @@
+package com.team1.etuser.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUniquePetsRes {
+    private Long petId;
+    private String img;
+}
+
