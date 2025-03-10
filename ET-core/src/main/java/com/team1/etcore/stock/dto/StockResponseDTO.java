4e3a73fa033d028f91b59b67a7637d4b18779db2
DrivenByCode <jaykey1004@gmail.com>
2025-03-10 10:38:28 +0900

    Refactor: 주식 데이터 초기 로딩 방식 변경

--- ET-core/src/main/java/com/team1/etcore/stock/dto/StockResponseDTO.java
+++ ET-core/src/main/java/com/team1/etcore/stock/dto/StockResponseDTO.java
@@ -1,11 +1,12 @@
 package com.team1.etcore.stock.dto;
 
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class StockResponseDTO {
+public class StockResponseDTO implements Serializable {
     private String code;
     private String name;
     private String img;
