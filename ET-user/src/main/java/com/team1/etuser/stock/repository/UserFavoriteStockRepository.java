79c0e02f5b635fb3edd1e5c8c767b058813d14f9
kwonjieon <holemart@naver.com>
2025-03-14 09:14:18 +0900

    fix : 관심종목 종가데이터 추가 수정

--- ET-user/src/main/java/com/team1/etuser/stock/repository/UserFavoriteStockRepository.java
+++ ET-user/src/main/java/com/team1/etuser/stock/repository/UserFavoriteStockRepository.java
@@ -19,6 +19,9 @@ public interface UserFavoriteStockRepository extends JpaRepository<UserFavoriteS
     @Query("SELECT new com.team1.etuser.stock.dto.StockClosePriceRes(u.stockCode, 0) " +
             "FROM UserStock u WHERE u.user = :user")
     List<StockClosePriceRes> findStockCodeByUser(@Param("user") User user);
+    @Query("SELECT new com.team1.etuser.stock.dto.StockClosePriceRes(u.stockCode, 0) " +
+            "FROM UserFavoriteStock u WHERE u.user = :user")
+    List<StockClosePriceRes> findFavoriteStockCodeByUser(@Param("user") User user);
 
     boolean existsByUserAndStockCode(User user, String stockCode);
 
